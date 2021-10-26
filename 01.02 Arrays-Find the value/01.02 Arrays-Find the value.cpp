5a70288fd3636cb49bf8ab8fba8c6df7816aba59
Jonathan Klukach Fernandez <jonathan.klukach.fernandez@gmail.com>
2021-10-26 22:00:34 +0200

    Création 01.02 String exercice 1

---
+++ 01.02 Arrays-Find the value/01.02 Arrays-Find the value.cpp
@@ -0,0 +1,10 @@
+/* Write a program that asks the user to input an integer V between 0 and 20. 
+The program creates an array of 10 random integer between 0 and 20, and writes out the content of the array and a message “V is / is not in the array” */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
